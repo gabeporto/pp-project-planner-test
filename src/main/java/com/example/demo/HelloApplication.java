91cf871a3fbc47232a7988e9f64ec977e3d245df
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-06-22 08:51:14 -0300

    JavaFX first commit

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class HelloApplication extends Application {
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.LIGHTBLUE);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
